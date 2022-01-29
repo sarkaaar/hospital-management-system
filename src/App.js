77ad6e2e61cf156f853624a317f2d7468c4b492c
sarkaaar <waliabdul144@gmail.com>
2022-01-29 18:27:08 +0500

    homepage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SignIn from "./pages/login";
 import AddDoctor from "./pages/addDoctor"
 import AddPatient from "./pages/addPatient"
 import ViewAppointments from "./pages/viewAppointments";
-// import Test from "./pages/Test"
+import Home from "./pages/home";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route exact path="/" element={<HomePage/>} /> */}
+        <Route exact path="/" element={<Home/>} />
         <Route exact path="/sign_in" element={<SignIn/>} />
         <Route exact path="/addDoctor" element={<AddDoctor/>} />
         <Route exact path="/addPatient" element={<AddPatient/>} />
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1 sx={{ display: "flex" }}>Welcome to The Hospital</h1>
+    </div>
+  );
+}
